65b65a1aece377b88fefca7d7d7c575217cc7c9c
Aanchol <aanchol.mushfika1234@gmail.com>
2022-02-05 22:53:49 +0600

    project rename

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:pashe_achi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const PasheAchiApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
