a6eb293858e3288836df273b4c156e0daa6ac1ab
Aanchol <aanchol.mushfika1234@gmail.com>
2022-02-11 16:33:19 +0600

    search field added

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pashe_achi/common_widget/homepage_container.dart';
 import 'package:pashe_achi/common_widget/homepg_iconcontent.dart';
+import 'package:pashe_achi/common_widget/search_field.dart';
 import 'package:pashe_achi/utils/constants.dart';
 
 class HomePage extends StatefulWidget {
@@ -52,23 +53,7 @@ class _HomePageState extends State<HomePage> {
                       SizedBox(
                         height: 20,
                       ),
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade100,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.black87,
-                              ),
-                              hintText: "Search you're looking for",
-                              hintStyle:
-                                  TextStyle(color: Colors.grey, fontSize: 15)),
-                        ),
-                      ),
+                      SearchField(searchIcon: Icons.search,),
                     ],
                   ),
                 )),
@@ -128,5 +113,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
 
 
---
+++ lib/common_widget/search_field.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SearchField extends StatelessWidget {
+  final IconData? searchIcon;
+
+  SearchField({@required this.searchIcon,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          borderRadius: BorderRadius.circular(15)),
+      child: TextField(
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            prefixIcon: Icon(searchIcon),
+            hintText: "Search you're looking for",
+            hintStyle:
+            TextStyle(color: Colors.grey, fontSize: 15)),
+      ),
+    );
+  }
+}
